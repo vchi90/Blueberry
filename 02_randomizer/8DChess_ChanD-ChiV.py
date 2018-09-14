06cd3105a2ae9b46965afe8b545295ee061ee6c6
Vincent Chi <vchi90@stuy.edu>
2018-09-13 21:14:44 -0400

    02_randomizer

---
+++ 02_randomizer/8DChess_ChanD-ChiV.py
@@ -0,0 +1,28 @@
+# 8DChess Derek Chan and Vincent Chi
+# SoftDev1 pd8
+# K02 -- NO-body expects the Spanish Inquisition!
+# 2018-09-12
+
+import random
+
+KREWES = {
+
+        'w': ['William Lu', 'Qian', 'Peter', 'Ahnaf', 'Kenny', 'Sophia', 'Sajed', 'Emily', 'Hasif', 'Brian ', 'Dennis', 'Jiayang', 'Shafali ', 'Isaac ', 'Tania', 'Derek', 'Shin', 'Vincent', 'Ricky', 'Puneet', 'Wei Wen', 'Tim', 'Jeffrey', 'Joyce ', 'Mohtasim', 'Simon', 'Thomas', 'Ray', 'Jack', 'Karen', 'Robin', 'Jabir', 'Johnny ', 'Matthew', 'Johnson Li', 'Angela', 'Crystal', 'Jiajie', 'Theodore (Dont really care)', 'Anton', 'Max', 'Bo', 'Andrew', 'Kendrick', 'Kevin', 'Kyle', 'Jamil', 'Mohammed', 'Ryan', 'Jason'],
+
+        'm': ['Daniel', 'Aleksandra', 'Addison', 'Hui Min', 'Aaron', 'Rubin', 'Raunak', 'Stefan', 'Cheryl', 'Cathy', 'Mai', 'Claire ', 'Alex', 'Bill', 'Daniel', 'Jason'],
+
+        'x': ['Derek', 'Britni', 'Joan', 'Vincent', 'Jared', 'Ivan', 'Thomas', 'Maggie', 'Damian', 'Tina', 'Fabiha', 'John', 'Susan ', 'Kaitlin', 'Michelle', 'Clara', 'Rachel', 'Amit', 'Jerry', 'Raymond', 'Zane', 'Soojin', 'Maryann', 'Adil', 'Josh', 'Imad']
+
+}
+
+def pick_someone():
+	return random.choice(KREWES[random.choice(list(KREWES.keys()))])
+
+#Code for testing
+# num_names = input('How many names are wanted?  ')
+# for count in range(int(num_names)):
+	# chosen_name = pick_someone()
+	# print(chosen_name)
+
+chosen_name = pick_someone()
+print(chosen_name)
