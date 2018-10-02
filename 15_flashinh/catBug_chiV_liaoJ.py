aef84833e5a7c6600942e11d7d6a75abad57ca81
Vincent Chi <vchi90@stuy.edu>
2018-10-02 18:13:00 -0400

    k15

---
+++ 15_flashinh/catBug_chiV_liaoJ.py
@@ -0,0 +1,51 @@
+#Team Catbug -- Vincent Chi, Joyce Liao
+#SoftDev1 pd8
+#K14 -- Do I Know You?
+#2018-10-01
+
+from flask import Flask, render_template, request, flash, \
+	session, url_for, redirect
+import os
+
+app = Flask(__name__)
+
+combo = { "softdev" : "period8"}
+
+app.secret_key = os.urandom(32)
+
+@app.route('/')
+def login():
+    if session != {}:
+        return redirect(url_for('welcome_user'))
+    return render_template("login.html")
+
+@app.route('/auth',methods=["POST"])
+def authenticate():
+    #print(request.form['username'])
+    if not(request.form['username'] in combo):
+        return render_template('error.html',
+                               msg = " That username does not exist!\"")
+    elif request.form['password'] != combo['softdev']:
+        return render_template('error.html',
+                               msg = " That is the wrong password!\"")
+    else:
+        flash('Welcome, Bravest Warrior!')
+        session[request.form['username']] = request.form['password']
+        return redirect(url_for('welcome_user'))
+
+
+@app.route('/welcome')
+def welcome_user():
+    return render_template("welcome.html",
+                           name = 'softdev')
+
+@app.route('/exit')
+def logout():
+    session.pop('softdev')
+    return redirect(url_for('login'))
+    
+
+if (__name__) == ("__main__"):
+    app.debug = True
+    app.run()
+
