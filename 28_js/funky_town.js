90edb2ce5ef0a0c6e0569848b9240492aebe8160
Vincent Chi <34114571+vchi90@users.noreply.github.com>
2018-12-19 20:19:28 -0500

    Update funky_town.js
    fix

--- 28_js/funky_town.js
+++ 28_js/funky_town.js
@@ -1,31 +1,30 @@
- // azrael - Jason Tung and Vincent Chi
+// azrael - Jason Tung and Vincent Chi
  // SoftDev1 pd8
  // K28 -- Sequential Progression
  // 2018-12-18
 
 
-var fib=function fibonacci(n){
-  if (n == 0){
-    return 0;
-  }
-  else if (n == 1 || n == 2){
-    return 1;
-  }
-  else{
-    return fibonacci(n-1) + fibonacci(n-2);
-  };
-}
-
-var gcd = (a,b)=> {
-    if (b == 0){
-        return a;
-    };
-    if (a < b){
-        return gcd(b,a);
-    };
-    return (a-b,b);
+var fibonacci = function(n){
+	if (n < 2)
+		return n;
+	else
+		return fibonacci(n - 1) + fibonacci(n - 2);
 };
 
-var randomStudent = (list) =>{
-  return list[Math.floor(Math.random()*list.length)];
+function gcd(a, b)
+{
+    temp = b;
+    while(b != 0)
+    {
+        temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+};
+
+students = ["dog", "cat", "tbm", "dw", "k"];
+
+var randomStudent = () =>{
+  return students[Math.floor(Math.random()*students.length)];
 };
